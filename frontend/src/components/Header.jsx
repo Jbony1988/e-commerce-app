a81a5602e8a754cb1f07938fe8f611b8cde1a5ca
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2023-05-06 13:48:30 -0500

    added header and footer

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Navbar, Nav, Container} from "react-bootstrap";
+import {FaShoppingCart, FaUser} from "react-icons/fa"
+
+ const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="./">
+                  E-commerce Application
+                </Navbar.Brand>
+                <Navbar.Toggle/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    
+                <Nav className="ms-auto justify-content-center">
+                    <Nav.Link href="/cart">
+                        <FaShoppingCart/>
+                        Cart
+                    </Nav.Link>
+                    <Nav.Link href="/cart">
+                        <FaUser/>
+                        Cart
+                    </Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
