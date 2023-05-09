c43f658e094aad0c0e2ddfef6bef76ad198c021b
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2023-05-08 22:59:43 -0500

    load products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
-        <h1>Welcome to the store</h1>;
+        <Container className="py-3">
+          <Outlet />
+        </Container>
       </main>
       <Footer />
     </>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import {Navbar, Nav, Container} from "react-bootstrap";
-import {FaShoppingCart, FaUser} from "react-icons/fa"
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
+import {LinkContainer} from "react-router-bootstrap"
 
- const Header = () => {
-    return (
-        <header>
-            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-            <Container>
-                <Navbar.Brand href="./">
-                  E-commerce Application
-                </Navbar.Brand>
-                <Navbar.Toggle/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    
-                <Nav className="ms-auto justify-content-center">
-                    <Nav.Link href="/cart">
-                        <FaShoppingCart/>
-                        Cart
-                    </Nav.Link>
-                    <Nav.Link href="/cart">
-                        <FaUser/>
-                        Cart
-                    </Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-            </Navbar>
-        </header>
-    )
-}
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+            <LinkContainer to="/">
 
-export default Header
+          <Navbar.Brand>
+            <img src={logo} alt="logo" />
+            E-commerce Application
+          </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto justify-content-center">
+              <Nav.Link href="/cart">
+                <FaShoppingCart />
+                Cart
+              </Nav.Link>
+              <Nav.Link href="/cart">
+                <FaUser />
+                Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
