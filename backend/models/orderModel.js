aff7a0ae2328c13a6c9078a5d3a9d5ef2204bd98
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2023-07-04 12:16:19 -0500

    finished seeder

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema(
+const orderSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
@@ -9,9 +9,9 @@ const orderSchema = new mongoose.Schema(
     },
     orderItems: [
       {
-        name: { type: "String", required: true },
+        name: { type: String, required: true },
         qty: { type: Number, required: true },
-        image: { type: "String", required: true },
+        image: { type: String, required: true },
         price: { type: Number, required: true },
         product: {
           type: mongoose.Schema.Types.ObjectId,
@@ -21,22 +21,20 @@ const orderSchema = new mongoose.Schema(
       },
     ],
     shippingAddress: {
-      address: {
-        type: "Strig",
-        required: true,
-      },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
     },
     paymentMethod: {
-      type: "String",
+      type: String,
       required: true,
     },
     paymentResult: {
-      id: { type: "String" },
-      status: { type: "String" },
-      update_time: {
-        type: "String",
-      },
-      email_address: { type: "String" },
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
     },
     itemsPrice: {
       type: Number,
@@ -61,7 +59,7 @@ const orderSchema = new mongoose.Schema(
     isPaid: {
       type: Boolean,
       required: true,
-      default: 0.0,
+      default: false,
     },
     paidAt: {
       type: Date,
@@ -71,7 +69,7 @@ const orderSchema = new mongoose.Schema(
       required: true,
       default: false,
     },
-    deliverAt: {
+    deliveredAt: {
       type: Date,
     },
   },
@@ -80,6 +78,6 @@ const orderSchema = new mongoose.Schema(
   }
 );
 
-const Order = new mongoose.model("Order", orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 
 export default Order;
