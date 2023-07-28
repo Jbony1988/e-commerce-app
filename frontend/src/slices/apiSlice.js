48dba291b4d24077af974228fe3980b8df34276b
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2023-07-28 15:21:41 -0300

    set up of redux slices

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { BASE_URL } from "../constants";
 
+// base url
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
 });
