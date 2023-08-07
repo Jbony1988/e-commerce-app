f9749013154494598ef4cfe2b1e410e4cb94870e
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2023-08-07 10:10:06 -0500

    added remove from cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import {useSelector} from "react-redux";
 import {LinkContainer} from "react-router-bootstrap"
 
 const Header = () => {
+
+  const {cartItems} = useSelector((state) => state.cart);
+
+  console.log(cartItems)
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -22,7 +27,13 @@ const Header = () => {
               <Nav.Link href="/cart">
                 <FaShoppingCart />
                 Cart
+                {cartItems.length > 0 && (
+                    <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
               </Nav.Link>
+             
               <Nav.Link href="/cart">
                 <FaUser />
                 Sign In
