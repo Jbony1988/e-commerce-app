d93d6351bf9406f2cce6fcedf9027e6f7028cb79
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2023-08-13 13:59:31 -0500

    added paypal button

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,97 +5,93 @@ import Order from "../models/orderModel.js";
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("Add Order Items");
-  //   const {
-  //     orderItems,
-  //     shippingAddress,
-  //     paymentMethod,
-  //     itemsPrice,
-  //     taxPrice,
-  //     shippingPrice,
-  //     totalPrice,
-  //   } = req.body;
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
 
-  //   if (orderItems && orderItems.length === 0) {
-  //     res.status(400);
-  //     throw new Error('No order items');
-  //   } else {
-  //     const order = new Order({
-  //       orderItems: orderItems.map((x) => ({
-  //         ...x,
-  //         product: x._id,
-  //         _id: undefined,
-  //       })),
-  //       user: req.user._id,
-  //       shippingAddress,
-  //       paymentMethod,
-  //       itemsPrice,
-  //       taxPrice,
-  //       shippingPrice,
-  //       totalPrice,
-  //     });
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map(x => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
 
-  //     const createdOrder = await order.save();
+    const createdOrder = await order.save();
 
-  //     res.status(201).json(createdOrder);
-  //   }
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("Get Orders");
-  //   const orders = await Order.find({ user: req.user._id });
-  //   res.json(orders);
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private/Admin
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("Get Order By ID");
-  //   const order = await Order.findById(req.params.id).populate(
-  //     "user",
-  //     "name email"
-  //   );
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
 
-  //   if (order) {
-  //     res.json(order);
-  //   } else {
-  //     res.status(404);
-  //     throw new Error("Order not found");
-  //   }
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to paid
-// @route   GET /api/orders/:id/pay
+// @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("Update order to paid");
-  //   const order = await Order.findById(req.params.id);
+  const order = await Order.findById(req.params.id);
 
-  //   if (order) {
-  //     order.isPaid = true;
-  //     order.paidAt = Date.now();
-  //     order.paymentResult = {
-  //       id: req.body.id,
-  //       status: req.body.status,
-  //       update_time: req.body.update_time,
-  //       email_address: req.body.payer.email_address,
-  //     };
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
 
-  //     const updatedOrder = await order.save();
+    const updatedOrder = await order.save();
 
-  //     res.json(updatedOrder);
-  //   } else {
-  //     res.status(404);
-  //     throw new Error("Order not found");
-  //   }
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to delivered
-// @route   GET /api/orders/:id/deliver
+// @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send("Update Order to deliver");
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,8 +20,11 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import OrderScreen from "./screens/OrderScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -35,6 +38,8 @@ const router = createBrowserRouter(
       <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
       </Route>
     </Route>
   )
@@ -44,7 +49,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
