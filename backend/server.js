6043100a538b20a3761550a9989e07cd84632454
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2023-09-18 16:12:50 -0300

    remove console.log api is running

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie Parser
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
