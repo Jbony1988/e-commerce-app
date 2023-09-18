8682f0e6a2ae5b2c1e81f3bcbf449a9670345fd4
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2023-09-18 16:08:07 -0300

    remove pack.json locked for permission issues

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie Parser
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -30,20 +34,21 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  // app.use("/uploads", express.static("/var/data/uploads"));
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   const __dirname = path.resolve();
+//   // app.use("/uploads", express.static("/var/data/uploads"));
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  //   const __dirname = path.resolve();
-  // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   //   const __dirname = path.resolve();
+//   // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+//   app.get("/", (req, res) => {
+//     res.send("API is running....");
+//   });
+// }
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(notFound);
 app.use(errorHandler);
